436b989f40b0ed02b1fc20da5108f43fb57b8c47
Joshua Bartling <joshbartling@yahoo.com>
2019-01-09 19:02:57 -0700

    finished first full iteration of wandering AI, updates will be made later

--- src/unity/First Room/Assets/BasicWalk.cs
+++ src/unity/First Room/Assets/BasicWalk.cs
@@ -9,57 +9,69 @@ public class BasicWalk : MonoBehaviour {
 	public Vector3 moveDir;
 	public LayerMask whatIsHittable;
 	public float maxDistFromWall = 0f;
-	public float idleTime = 0f;
+	public float idleTime = 3.0f;
+	public float walkTime = 4.0f;
 
 	// Use this for initialization
 	void Start () {
 
 		rbody = GetComponent<Rigidbody>();
 		moveDir = ChooseDirection();
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		rbody.velocity = moveDir * moveForce;
-
-		/*if(Physics.Raycast(transform.position, transform.forward, maxDistFromWall, whatIsHittable))
+		if(walkTime > 0f)
 		{
-			StartCoroutine(Wait());
-			moveDir = ChooseDirection();
-			transform.rotation = Quaternion.LookRotation(moveDir);
-		} */
+			Debug.Log("Good");
+			//Debug.Log("Time1: " + walkTime.ToString());
+			walkTime -= Time.deltaTime;
+			Debug.Log("Time2: " + walkTime.ToString());
+			//rbody.velocity = moveDir * moveForce;
+		}
+		else
+		{
+			Debug.Log("Bad");
+			//walkTime = 5.0f;
+			moveDir.Set(0,0,0);
+			if(idleTime > 0f)
+			{
+				idleTime -= Time.deltaTime;
+				//rbody.velocity = moveDir * moveForce;
+			}
+			else{
+				walkTime = 4.0f;
+				idleTime = 3.0f;
+				moveDir = ChooseDirection();
+				transform.rotation = Quaternion.LookRotation(moveDir);
+			}
+		}
+		rbody.velocity = moveDir * moveForce;
 	}
 
 	Vector3 ChooseDirection()
 	{
-		System.Random ran = new System.Random();
-		Vector3 temp = new Vector3(ran.Next(-5,5), 0, ran.Next(-5,5));
+		Vector3 temp = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
 		Vector3 check = new Vector3(0,0,0);
 		if(temp == check){
 			Debug.Log("Hit 0,0,0");
 			temp.Set(0,0,1);
 		}
-		if(moveDir.x == temp.x){
-			temp.Set(-temp.x, 0, temp.z);
+		if(moveDir.x >= (temp.x-5) && moveDir.x <= (temp.x+5)){
+			return ChooseDirection();
+			//temp.Set(-temp.x, 0, temp.z);
 		}
-		if(moveDir.z == temp.z){
-			temp.Set(temp.z, 0, -temp.z);
+		if(moveDir.z >= (temp.z-5) && moveDir.z <= (temp.z+5)){
+			//temp.Set(temp.z, 0, -temp.z);
+			return ChooseDirection();
 		}
 		return temp;
 	}
 
 	void OnCollisionEnter(Collision col)
 	{
-
+		walkTime = 4.0f;
 		moveDir = ChooseDirection();
 		transform.rotation = Quaternion.LookRotation(moveDir);
-
-	}
-
-	IEnumerator Wait(){
-		yield return new WaitForSeconds(5);
-		Debug.Log("Done");
 	}
 }
