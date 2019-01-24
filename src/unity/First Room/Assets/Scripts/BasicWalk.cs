b18c55a7d83139b8ecd2fe068693a62cf46d37c8
Joshua Bartling <joshbartling@yahoo.com>
2019-01-24 11:18:15 -0700

    Sorry kurt...

--- src/unity/First Room/Assets/Scripts/BasicWalk.cs
+++ src/unity/First Room/Assets/Scripts/BasicWalk.cs
@@ -4,51 +4,46 @@ using UnityEngine;
 
 public class BasicWalk : MonoBehaviour {
 
-	private GameObject manager;
 	public float moveForce = 0f;
 	private Rigidbody rbody;
-	private PathFinder pathFinder;
-	private Grid grid;
 	public Vector3 moveDir;
 	public LayerMask whatIsHittable;
 	public float maxDistFromWall = 0f;
 	public float idleTime = 3.0f;
 	public float walkTime = 4.0f;
+	public bool isEnabled = true;
 
 	// Use this for initialization
 
 	void Start () {
-		Debug.Log("IM IN START");
-		manager = GameObject.Find("GameManager");
 		rbody = GetComponent<Rigidbody>();
-		pathFinder = manager.GetComponent<PathFinder>();
-		grid = manager.GetComponent<Grid>();
 		moveDir = ChooseDirection();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		if(!isEnabled)
+		{
+			return;
+		}
 		if(walkTime > 0f)
 		{
 			walkTime -= Time.deltaTime;
 		}
 		else
 		{
-			//Debug.Log("Bad");
-			//walkTime = 5.0f;
 			moveDir.Set(0,0,0);
 			if(idleTime > 0f)
 			{
 				idleTime -= Time.deltaTime;
-				//rbody.velocity = moveDir * moveForce;
 			}
 			else{
 				walkTime = 4.0f;
 				idleTime = 3.0f;
-				WalkTo();
-				moveDir = ChooseDirection();
-				transform.rotation = Quaternion.LookRotation(moveDir);
+				moveDir.Set(0f, 0f, 0f);
+				//This is for the test, remove later.
+				isEnabled = false;
 			}
 		}
 		rbody.velocity = moveDir * moveForce;
@@ -59,15 +54,12 @@ public class BasicWalk : MonoBehaviour {
 		Vector3 temp = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
 		Vector3 check = new Vector3(0,0,0);
 		if(temp == check){
-			//Debug.Log("Hit 0,0,0");
 			temp.Set(0,0,1);
 		}
 		if(moveDir.x >= (temp.x-5) && moveDir.x <= (temp.x+5)){
 			return ChooseDirection();
-			//temp.Set(-temp.x, 0, temp.z);
 		}
 		if(moveDir.z >= (temp.z-5) && moveDir.z <= (temp.z+5)){
-			//temp.Set(temp.z, 0, -temp.z);
 			return ChooseDirection();
 		}
 		return temp;
@@ -80,7 +72,7 @@ public class BasicWalk : MonoBehaviour {
 		transform.rotation = Quaternion.LookRotation(moveDir);
 	}
 
-	void WalkTo()
+	/*void WalkTo()
 	{
 		if(pathFinder != null)
 		{
@@ -95,7 +87,7 @@ public class BasicWalk : MonoBehaviour {
 		}
 		
 		//StartCoroutine(AsyncWalk());
-	}
+	}*/
 	/*IEnumerator AsyncWalk(){
 		if(pathFinder != null)
 		{
--- src/unity/First Room/Assets/Scripts/aStar.cs
+++ src/unity/First Room/Assets/Scripts/aStar.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class aStar : MonoBehaviour
 {
+    private GameObject manager;
+    private PathFinder pathFinder;
+	private Grid grid;
+    private BasicWalk walk;
     // Start is called before the first frame update
     void Start()
     {
-        
+        manager = GameObject.Find("GameManager");
+        pathFinder = manager.GetComponent<PathFinder>();
+		grid = manager.GetComponent<Grid>();
+        walk = GetComponent<BasicWalk>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(walk.isEnabled)
     }
 }
