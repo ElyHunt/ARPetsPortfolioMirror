7574c8b68280913734e0c260ce3ff77c6e093bb2
Joshua Bartling <joshbartling@yahoo.com>
2019-02-19 20:22:58 -0700

    First behavior linked, Add player and game menues

---
+++ src/unity/First Room/Assets/Scripts/Activity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activity : MonoBehaviour
+{
+    GameObject cat;
+    GameObject location;
+    aStar goal;
+    BasicWalk trigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cat = GameObject.Find("Cat Lite");
+        goal = cat.GetComponent<aStar>();
+        trigger = cat.GetComponent<BasicWalk>();
+    }
+
+    public void feed(){
+        location = GameObject.Find("bowl (1)");
+        goal.destination = location.transform;
+        trigger.isEnabled = false;
+        return;
+    }
+}
