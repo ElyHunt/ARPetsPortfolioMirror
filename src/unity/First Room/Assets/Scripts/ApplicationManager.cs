b83e3600278d0bae40b130c30dd3a5263ec9c528
Elliot Hunt <jku@elys-mbp.uwyo.edu>
2019-02-25 16:02:46 -0700

    Dummy Continue Button Function in 3DMenu Scene. ApplicationManager script change.

--- src/unity/First Room/Assets/Scripts/ApplicationManager.cs
+++ src/unity/First Room/Assets/Scripts/ApplicationManager.cs
@@ -3,8 +3,24 @@ using System.Collections;
 
 public class ApplicationManager : MonoBehaviour {
 	
+    public void Continue ()
+    {
+        Debug.Log("CLICKED CONTINUE YAY");
 
-	public void Quit () 
+    }//Stub for Continue.
+
+    //Functionality added via Inspector for object
+        // > On Click()
+            // + Button
+                // Add Application Manager from Scene
+                // Editor and Runtime
+                // ApplicationManager.Continue
+                // SOMETHING LIKE THIS!
+                // Now just add read and write to file.
+                // Can be ported over to the main project
+                // rather than demo scene for menu. Cool!
+                //  - E.H.
+    public void Quit () 
 	{
 		#if UNITY_EDITOR
 		UnityEditor.EditorApplication.isPlaying = false;
