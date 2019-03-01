b6a02c27309011efe98f1ea759aa41536e1da050
Joshua Bartling <joshbartling@yahoo.com>
2019-02-28 18:21:29 -0700

    Finished save. Left open for future development. Enables multiple loads

---
+++ src/unity/First Room/Assets/Scripts/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Animal
+{
+    public string name;
+    public int happiness;
+    public int age;
+    public int hunger;
+
+    public Animal () {
+        this.name = "Foxy";
+        this.happiness = 50;
+        this.age = 0;
+        this.hunger = 0;
+    }
+}
---
+++ src/unity/First Room/Assets/Scripts/Game.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Game
+{
+   public static Game current;
+   public Animal cat;
+
+   public Game () {
+       cat = new Animal();
+   }
+}
